e6c5e07479111549a8c47ae843a9d5d9c0e5dc77
gen <kyliki@yandex.ru>
2025-08-31 09:07:25 +0300

    groupsTable layout

--- src/pages/camps/camp/sections/GroupsSection.tsx
+++ src/pages/camps/camp/sections/GroupsSection.tsx
@@ -1,3 +1,19 @@
+import GroupsTable from "@/pages/camps/camp/components/GroupsTable.tsx";
+import { useParams } from "react-router-dom";
+
+const GROUP_LIST_TEST = [
+  {
+    id: 1,
+    name: "old",
+  },
+  {
+    id: 2,
+    name: "new",
+  },
+];
+
 export default function GroupsSection() {
-  return <div>Groups</div>;
+  const { campId } = useParams();
+
+  return <GroupsTable campId={Number(campId)} list={GROUP_LIST_TEST} />;
 }
