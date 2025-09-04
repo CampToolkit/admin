3dc7279454e49297192bd1fb66e5f462667c3813
TimPulin <tigeronsun@gmail.com>
2025-09-04 09:22:12 +0300

    formSwitcher in AddPersonToCampButton

---
+++ src/pages/camps/camp/components/add-to-camp-modal-layout/FormSwitcherLayout.tsx
@@ -0,0 +1,54 @@
+import { Button, Grid, Stack } from "@mui/material";
+import { type ReactNode, useState } from "react";
+
+interface Component {
+  key: string;
+  label: string;
+  element: ReactNode;
+}
+
+interface Props {
+  components: Component[];
+}
+
+export default function AddToCampLayout(props: Props) {
+  const { components } = props;
+  const [activeKey, setActiveKey] = useState<string>(components[0].key);
+
+  const currentComponent = components.find((c) => c.key === activeKey);
+
+  return (
+    <Grid
+      container
+      sx={{
+        height: "100%",
+      }}
+    >
+      <Grid size={2} border={1}>
+        <Stack>
+          {components.map((component) => (
+            <Button
+              key={component.key}
+              onClick={() => setActiveKey(component.key)}
+            >
+              {component.label}
+            </Button>
+          ))}
+        </Stack>
+      </Grid>
+      <Grid size={8} border={1}>
+        {currentComponent?.element}
+      </Grid>
+      <Grid size={2} border={1}>
+        <Stack>
+          <Button form={activeKey} type="submit">
+            Сохранить
+          </Button>
+          <Button form={activeKey} type="reset">
+            Очистить
+          </Button>
+        </Stack>
+      </Grid>
+    </Grid>
+  );
+}
