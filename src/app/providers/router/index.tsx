1c287adb9e0ce457a60029202104bbb850e29c22
gen <kyliki@yandex.ru>
2025-09-06 02:03:13 +0300

    delete unused forms

--- src/app/providers/router/index.tsx
+++ src/app/providers/router/index.tsx
@@ -3,35 +3,6 @@ import HomePage from "@/pages/home/HomePage.tsx";
 import MainLayout from "@/shared/components/layouts/MainLayout.tsx";
 import CampsPage from "@/pages/camps/CampsPage";
 import CampPage from "@/pages/camps/CampPage.tsx";
-import GroupsForm from "@/pages/camps/camp/forms/GroupsForm.tsx";
-
-import BaseInfoSection from "@/pages/camps/camp/sections/BaseInfoSection.tsx";
-import LocationsSection from "@/pages/camps/camp/sections/LocationsSection.tsx";
-import SportsmenSection from "@/pages/camps/camp/sections/SportsmenSection.tsx";
-import ScheduleSection from "@/pages/camps/camp/sections/ScheduleSection.tsx";
-
-const campChildren = [
-  {
-    path: "base-info",
-    element: <BaseInfoSection />,
-  },
-  {
-    path: "groups",
-    element: <GroupsForm />,
-  },
-  {
-    path: "sportsmen",
-    element: <SportsmenSection />,
-  },
-  {
-    path: "locations",
-    element: <LocationsSection />,
-  },
-  {
-    path: "schedule",
-    element: <ScheduleSection />,
-  },
-];
 
 const router = createBrowserRouter([
   {
